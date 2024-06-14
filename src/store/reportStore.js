fb9316dfe55930450b1923796c94c75f6fe80064
shanghanrun <idim7@naver.com>
2024-06-15 01:25:18 +0900

    .

--- src/store/reportStore.js
+++ src/store/reportStore.js
@@ -4,7 +4,7 @@ import api from '../utils/api';
 const reportStore = create((set, get)=>({
 	reportList:[],
 	createReport:async(user, assignId, domain, frontEnd, backEnd, comment)=>{
-		const userId = user._id,
+		const userId = user._id
 		const userName = user.name
 		try{
 			const resp = await api.post('/report', {userId, userName, assignId, domain, frontEnd, backEnd, comment})
